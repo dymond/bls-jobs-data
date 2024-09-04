38d7f5b9866060c7a2c64c29930f6e0d73f0268a
Cory Dymond <hi@corydymond.com>
2024-09-04 09:28:21 -0400

    it

--- workflows/bls.js
+++ workflows/bls.js
@@ -15,7 +15,7 @@ module.exports = async function ({helpers}){
 							`https://api.bls.gov/publicAPI/v2/timeseries/data/${degreeData[key]}?latest=true&registrationkey=${env_secrets.BLS_KEY2}`
 						).then(function (response){
 							if (response.status === 200) {
-								console.log(response.Results);
+								console.log(response.data);
 								const hourlyWage = response.data.Results.series[0]?.data[0]?.value;
 								// helpers.axios.patch( teableUrl, { hourlyWage: hourlyWage }, { headers: { "Authorization": `Bearer ${env_secrets.TEABLE_KEY}` }});
 							} else if (response.status === 429) {
