b658e4e8b0b96161244d36725b283d30694020af
Cory Dymond <hi@corydymond.com>
2024-09-04 10:14:50 -0400

    it

--- workflows/bls.js
+++ workflows/bls.js
@@ -29,7 +29,7 @@ module.exports = async function ({helpers}){
 			{ "seriesid": Object.values(allSeriesIds), "registrationkey": env_secrets.BLS_KEY2, latest:true }
 		).then(function (response){
 			if (response.status === 200) {
-				const hourlyWage = response.data;
+				const hourlyWage = response.data.Results.series;
 				console.log(hourlyWage);
 				// helpers.axios.patch( teableUrl, { hourlyWage: hourlyWage }, { headers: { "Authorization": `Bearer ${env_secrets.TEABLE_KEY}` }});
 			}
