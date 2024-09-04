f04b19938ea150c0d34b4565be356c214addf6a2
Cory Dymond <hi@corydymond.com>
2024-09-04 09:09:35 -0400

    it

--- workflows/bls.js
+++ workflows/bls.js
@@ -1,6 +1,7 @@
 module.exports = async function ({helpers}){
+	let blsCount = 0;
 	const env_secrets = JSON.parse(process.env['INPUT_JSON-SECRETS']);
-	console.log(env_secrets);
+	// console.log(env_secrets);
 	const teableUrl = "https://app.teable.io/api/table/tblsWx24MUhM7JxkMNx/record";
 	helpers.axios.get(teableUrl, { headers: { "Authorization": `Bearer ${env_secrets.TEABLE_KEY}` } })
 	.then((response) => {
@@ -11,15 +12,20 @@ module.exports = async function ({helpers}){
                 if (key === 'hourlySeriesId') {
 					if ( degreeData[key] ) {
 						console.log(`${degreeData['degreeCode']}: ${degreeData[key]}`);
-						helpers.axios.get(
-							`https://api.bls.gov/publicAPI/v2/timeseries/data/${degreeData[key]}?latest=true&registrationkey=${env_secrets.BLS_KEY2}`
-						).then(function (response) {
-							const hourlyWage = response.data.Results.series[0]?.data[0]?.value;
-							helpers.axios.patch( teableUrl, { hourlyWage: hourlyWage }, { headers: { "Authorization": `Bearer ${env_secrets.TEABLE_KEY}` }});
-						})
-						.catch(function (error) {
-							console.log(error);
-						});
+						if ( blsCount < 1 ) {
+							helpers.axios.get(
+								`https://api.bls.gov/publicAPI/v2/timeseries/data/${degreeData[key]}?latest=true&registrationkey=${env_secrets.BLS_KEY2}`
+							).then(function (response) {
+								++blsCount;
+								console.log(response);
+								const hourlyWage = response.data.Results.series[0]?.data[0]?.value;
+								// helpers.axios.patch( teableUrl, { hourlyWage: hourlyWage }, { headers: { "Authorization": `Bearer ${env_secrets.TEABLE_KEY}` }});
+							})
+							.catch(function (error) {
+								++blsCount;
+								console.log(error);
+							});
+						}
 					}
                 }
             })
