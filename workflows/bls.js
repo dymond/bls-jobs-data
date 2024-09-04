68588d813d7f70e7ec28acf3636b27710ad4a318
Cory Dymond <hi@corydymond.com>
2024-09-04 08:36:33 -0400

    it

--- workflows/bls.js
+++ workflows/bls.js
@@ -6,6 +6,8 @@ module.exports = async function ({helpers}){
 		const records = response.data.records;
         records.forEach(record => {
 			Object.entries(record.fields).forEach(field => {
+				console.log('YYYYYYYYYYY');
+				console.log(field);
                 if (field.hourlySeriesId) {
                     helpers.axios.get(
                         `https://api.bls.gov/publicAPI/v2/timeseries/data/${field.hourlySeriesId}?latest=true&registrationkey=${env_secrets.BLS_KEY}`
