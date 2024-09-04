0475b02490660dd1501399d483764c24fb2b7cec
Cory Dymond <hi@corydymond.com>
2024-09-04 07:59:14 -0400

    it

--- workflows/bls.js
+++ workflows/bls.js
@@ -1,5 +1,5 @@
 module.exports = async function ({helpers}){
-	console.info(process.env);
+	console.info(process);
 	const teableUrl = "https://app.teable.io/api/table/tblsWx24MUhM7JxkMNx/record";
 	helpers.axios.get(teableUrl, { headers: { "Authorization": `Bearer ${process.env.TEABLE_KEY}` } })
 	.then((response) => {
