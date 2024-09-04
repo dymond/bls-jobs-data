b2ca067703be9728546da41691f8497e46517f70
Cory Dymond <hi@corydymond.com>
2024-09-04 09:46:07 -0400

    it

--- workflows/bls.js
+++ workflows/bls.js
@@ -1,6 +1,8 @@
 module.exports = async function ({helpers}){
 	const env_secrets = JSON.parse(process.env['INPUT_JSON-SECRETS']);
-	const teableUrl = "https://app.teable.io/api/table/tblsWx24MUhM7JxkMNx/record";
+	const blsUrl = 'https://api.bls.gov/publicAPI/v2/timeseries/data/';
+	const teableUrl = 'https://app.teable.io/api/table/tblsWx24MUhM7JxkMNx/record';
+	let allSeriesIds = [];
 	helpers.axios.get(teableUrl, { headers: { "Authorization": `Bearer ${env_secrets.TEABLE_KEY}` } })
 	.then(async (response) => {
 		const records = response.data.records;
@@ -9,26 +11,27 @@ module.exports = async function ({helpers}){
 			for (const key of Object.keys(degreeData)) {
 				if (key === 'hourlySeriesId') {
 					if (degreeData[key]) {
-						let wait = 0;
-						if (wait) await new Promise((resolve) => setTimeout(resolve, wait));
-						helpers.axios.get(
-							`https://api.bls.gov/publicAPI/v2/timeseries/data/${degreeData[key]}?latest=true&registrationkey=${env_secrets.BLS_KEY2}`
-						).then(function (response){
-							if (response.status === 200) {
-								const hourlyWage = response.data.Results.series[0]?.data[0]?.value;
-								console.log(hourlyWage);
-								helpers.axios.patch( teableUrl, { hourlyWage: hourlyWage }, { headers: { "Authorization": `Bearer ${env_secrets.TEABLE_KEY}` }});
-							} else if (response.status === 429) {
-								wait = wait + 10000
-							}
-						}).catch(function (error){
-							console.log(error);
-						});
+						allSeriesIds.push(degreeData[key]);
 					}
 				}
 			}
 		}
 	}).catch(function (error){
 		console.log(error);
+	}).then(function (result) {
+		console.log(allSeriesIds);
+		helpers.axios.post(
+			blsUrl,
+			{ "seriesid": seriesIds, "registrationkey": env_secrets.BLS_KEY2, latest:true }
+		).then(function (response){
+			if (response.status === 200) {
+				const hourlyWage = response.data
+				console.log(hourlyWage);
+				// helpers.axios.patch( teableUrl, { hourlyWage: hourlyWage }, { headers: { "Authorization": `Bearer ${env_secrets.TEABLE_KEY}` }});
+			}
+		}).catch(function (error){
+			console.log(error);
+		});
 	});
+
 };
